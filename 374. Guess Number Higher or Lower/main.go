35f40394b8acc75d65f3eee935c7d226dedb2a34
said7388 <mdabusaidriyaz@gmail.com>
2022-09-13 20:06:12 +0600

    first commit

---
+++ 374. Guess Number Higher or Lower/main.go
@@ -0,0 +1,45 @@
+/*
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+*/
+
+package main
+
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * func guess(num int) int;
+ */
+
+func guessNumber(n int) int {
+	left := 1
+	right := n
+
+	for left <= right {
+		mid := (left + right) / 2
+		g := guess(mid)
+
+		if g == 0 {
+			return mid
+		} else if 0 > g {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return 0
+}
---
+++ 704. Binary Search/704-Binary_Search.go
@@ -0,0 +1,25 @@
+/* Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+package main
+
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return -1
+
+}
